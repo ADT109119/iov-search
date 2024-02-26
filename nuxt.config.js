6b828a09b53afe3efc28262cbd07e8c6577eefff
ADT109119 <2.jerry32262686@gmail.com>
2024-02-26 21:17:43 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+let development = process.env.NODE_ENV !== 'production'
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -47,10 +49,14 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: './'
+    baseURL: '/'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  app: {
+    baseURL: development ? "/":"iov-search"
   }
+  
 }
